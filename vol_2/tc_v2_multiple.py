535396896e2abd362816ff7411c5119af47cafe5
emre devrim <edevrim92@gmail.com>
2019-05-03 08:59:38 +0200

    Add files via upload

--- vol_2/tc_v2_multiple.py
+++ vol_2/tc_v2_multiple.py
@@ -23,8 +23,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_curve, roc_auc_score, auc, accuracy_score, confusion_matrix
 #%%
 #take datasets
-negatives = pd.read_excel("negatives.xlsx") 
-count1 = negatives['source'].value_counts()
+#negatives = pd.read_excel("negatives.xlsx") 
+#count1 = negatives['source'].value_counts()
 
 #to upload github, first x rows have been taken
 #writer = pd.ExcelWriter('negatives_sample.xlsx', engine='xlsxwriter');
@@ -90,45 +90,48 @@ safecity4 = safecity4.rename(columns={'text2': 'text'}).reset_index(drop=True)
 
 #%%
 del safecity1, safecity2, safecity3
-del keep_columns, count1, count2
+del keep_columns, 
+#del count1
+del count2
 del safecity_new
 
 #%%
 #target 0's
-count2 = negatives['source'].value_counts()
-
-negatives['text'] = negatives.astype(str)
-negatives['len_1'] = negatives.apply(lambda x: len(x['text'].strip()) if len(x['text'].strip()) > 0 else 0, axis=1) 
-
-#remove less than 30 chars
-negatives2 = negatives[negatives.len_1 >= 30]
-
-#sampled for now (to make it ~ 1/2)
-negatives3 = negatives2[negatives2['source'] == 'imdb_2'].sample(n=7000, random_state=1905)
-negatives4 = negatives2[negatives2['source'] != 'imdb_2']
-
-negatives5 = negatives3.append(negatives4, ignore_index=True)
-negatives5 = negatives5[['text', 'target_harassment']].drop_duplicates()
-
-#remove weird chars
-negatives5['text2'] = negatives5['text'].apply(lambda x: ''.join([" " if ord(i) < 32 or ord(i) > 126 else i for i in x]))
-negatives5['len_2'] = negatives5.apply(lambda x: len(x['text2'].strip()) if len(x['text2'].strip()) > 0 else 0, axis=1) 
-
-negatives6 = negatives5.copy()
-negatives6['VERBAL ABUSE'] = 0
-negatives6['NON-VERBAL ABUSE'] = 0
-negatives6['PHYSICAL ABUSE'] = 0
-negatives6['SERIOUS PHYSICAL ABUSE'] = 0
-negatives6['OTHER ABUSE'] = 0
-
-negatives6 = negatives6[['text2', 'VERBAL ABUSE', 'NON-VERBAL ABUSE',
-       'PHYSICAL ABUSE', 'SERIOUS PHYSICAL ABUSE', 'OTHER ABUSE']]
-
-negatives6 = negatives6.rename(columns={'text2': 'text'}).reset_index(drop=True)
+#count2 = negatives['source'].value_counts()
+#
+#negatives['text'] = negatives.astype(str)
+#negatives['len_1'] = negatives.apply(lambda x: len(x['text'].strip()) if len(x['text'].strip()) > 0 else 0, axis=1) 
+#
+##remove less than 30 chars
+#negatives2 = negatives[negatives.len_1 >= 30]
+#
+##sampled for now (to make it ~ 1/2)
+#negatives3 = negatives2[negatives2['source'] == 'imdb_2'].sample(n=7000, random_state=1905)
+#negatives4 = negatives2[negatives2['source'] != 'imdb_2']
+#
+#negatives5 = negatives3.append(negatives4, ignore_index=True)
+#negatives5 = negatives5[['text', 'target_harassment']].drop_duplicates()
+#
+##remove weird chars
+#negatives5['text2'] = negatives5['text'].apply(lambda x: ''.join([" " if ord(i) < 32 or ord(i) > 126 else i for i in x]))
+#negatives5['len_2'] = negatives5.apply(lambda x: len(x['text2'].strip()) if len(x['text2'].strip()) > 0 else 0, axis=1) 
+#
+#negatives6 = negatives5.copy()
+#negatives6['VERBAL ABUSE'] = 0
+#negatives6['NON-VERBAL ABUSE'] = 0
+#negatives6['PHYSICAL ABUSE'] = 0
+#negatives6['SERIOUS PHYSICAL ABUSE'] = 0
+#negatives6['OTHER ABUSE'] = 0
+#
+#negatives6 = negatives6[['text2', 'VERBAL ABUSE', 'NON-VERBAL ABUSE',
+#       'PHYSICAL ABUSE', 'SERIOUS PHYSICAL ABUSE', 'OTHER ABUSE']]
+#
+#negatives6 = negatives6.rename(columns={'text2': 'text'}).reset_index(drop=True)
 
 #%%
 
-data_all = safecity4.append(negatives6, ignore_index=True)
+#data_all = safecity4.append(negatives6, ignore_index=True)
+data_all = safecity4
 data_all = data_all.reset_index(drop=True)
 
 va = pd.crosstab(index = data_all['VERBAL ABUSE'], columns="Total count")
@@ -150,12 +153,14 @@ pa = pd.crosstab(index = data_all['PHYSICAL ABUSE'], columns="Total count")
 #writer.save();
 
 #%%
-del negatives, negatives2, negatives3, negatives4, negatives5, negatives6
-del count2, safecity4
+#del negatives, negatives2, negatives3, negatives4, negatives5, negatives6
+#del count2
+del safecity4
 del va, nva, pa, spa, oa
 
 #%%
-#train - test split 
+#train - test split
+#CLASSIFYING ONLY AMONG HARASSMENT CASES  
 
 def per_abuse(data1, target):
     
@@ -165,7 +170,7 @@ def per_abuse(data1, target):
     positives = data2[data2[target] == 1]
     len1 = len(positives)
     
-    negatives = data2[data2[target] == 0].sample(n=len1, random_state=1905)
+    negatives = data2[data2[target] == 0].sample(n=len1, random_state=1905, replace=True)
     
     data3 = positives.append(negatives, ignore_index=True)
     data3 = data3.reset_index(drop=True)
